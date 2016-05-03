ada487e181ea2d5ac1b15bf7bd07f47367e84475
Luping Yu <lazydingding@gmail.com>
2016-05-03 17:08:52 +0100

    Added files via upload

--- src/uk/ac/bris/cs/databases/web/Server.java
+++ src/uk/ac/bris/cs/databases/web/Server.java
@@ -1,5 +1,5 @@
 /*
- * Mini implementation forum server and UI. 
+ * Mini implementation forum server and UI.
  */
 package uk.ac.bris.cs.databases.web;
 
@@ -17,14 +17,14 @@ import uk.ac.bris.cs.databases.cwk3.API;
  * @author csxdb
  */
 public class Server extends RouterNanoHTTPD {
-    
+
     private static final String DATABASE = "jdbc:sqlite:database/database.sqlite3";
 
     public Server() {
         super(8000);
         addMappings();
     }
-    
+
     @Override public void addMappings() {
         super.addMappings();
         addRoute("/person/:id", PersonHandler.class);
@@ -38,32 +38,32 @@ public class Server extends RouterNanoHTTPD {
         addRoute("/forum2/:id", AdvancedForumHandler.class);
         addRoute("/topic/:id", TopicHandler.class);
         addRoute("/topic0/:id", SimpleTopicHandler.class);
-        
+
         addRoute("/newforum", NewForumHandler.class);
         addRoute("/createforum", CreateForumHandler.class);
-        
+
         addRoute("/newtopic/:id", NewTopicHandler.class);
         addRoute("/createtopic", CreateTopicHandler.class);
-        
+
         addRoute("/newpost/:id", NewPostHandler.class);
         addRoute("/createpost", CreatePostHandler.class);
-        
+
         addRoute("/newperson", NewPersonHandler.class);
         addRoute("/createperson", CreatePersonHandler.class);
 
         addRoute("/login", LoginHandler.class);
         addRoute("/login/:id", LoginHandler.class);
-        
+
         addRoute("/styles.css", StyleHandler.class, "resources/styles.css");
         addRoute("/gridlex.css", StyleHandler.class, "resources/gridlex.css");
     }
-    
+
     public static void main(String[] args) throws Exception {
-        
+
         ApplicationContext c = ApplicationContext.getInstance();
 
         // database //
-        
+
         Connection conn;
         try {
             conn = DriverManager.getConnection(DATABASE);
@@ -75,14 +75,14 @@ public class Server extends RouterNanoHTTPD {
         }
 
         // templating //
-        
+
         Configuration cfg = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
         cfg.setDirectoryForTemplateLoading(new File("resources/templates"));
         cfg.setDefaultEncoding("UTF-8");
         c.setTemplateConfiguration(cfg);
-        
+
         // server //
-        
+
         Server server = new Server();
         ServerRunner.run(Server.class);
     }
